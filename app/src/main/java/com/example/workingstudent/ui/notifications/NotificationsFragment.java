0ee9130b8eaccf2b3c4e5fe8a04d7eeb81e1bec2
Vorname Name <cwild@studenten.hs-bremerhaven.de>
2021-02-24 16:12:28 +0100

    Über die App und E-Mail versenden funktioniert

--- app/src/main/java/com/example/workingstudent/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/workingstudent/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,7 @@
 package com.example.workingstudent.ui.notifications;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,16 +15,57 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.workingstudent.R;
+import com.example.workingstudent.ui.edittime.editTime;
+import com.example.workingstudent.ui.überDieApp.UeberDieApp;
 
 public class NotificationsFragment extends Fragment {
 
-
+    TextView sendEmail;
+    TextView ueberDieApp;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        sendEmail = root.findViewById(R.id.email_view);
+        ueberDieApp = root.findViewById(R.id.über_die_appTextView);
+
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
+
+        ueberDieApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getContext(), UeberDieApp.class);
+                startActivity(myIntent);
+            }
+        });
+
 
         return root;
+
+
+    }
+
+
+    public void sendEmail() {
+
+        try {
+            String[] adress = {"christian-wild@hotmail.de"};
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_EMAIL, adress);
+
+            startActivity(intent);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/workingstudent/ui/überDieApp/UeberDieApp.java
@@ -0,0 +1,17 @@
+package com.example.workingstudent.ui.überDieApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.workingstudent.R;
+
+public class UeberDieApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ueber_die_app);
+
+    }
+}
